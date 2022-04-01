8fb29061a19736cf6ff444a94705bde003aeeff9
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-01 11:53:47 +0530

    Version : 1.0.0 : Initial Commit.

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+/*
+Website Name - Form Validation
+Name : Vanshul Kesharwani
+Date : 01/04/2022
+Version : 1.0.0
+Email : vkvanshulkesharwani54@gmail.com
+Description : This is a Web app for validating a form with Regular Expression of JavaScript.
+*/
