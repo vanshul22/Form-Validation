73861750a7cb358617c12981b71aef73c94f8eba
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-01 13:01:52 +0530

    Version 2.2.1 : Added Section form in index.html.

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 Website Name - Form Validation
 Name : Vanshul Kesharwani
 Date : 01/04/2022
-Version : 2.1.0
+Version : 2.2.1
 Email : vkvanshulkesharwani54@gmail.com
 Description : This is a Web app for validating a form with Regular Expression of JavaScript.
 */
