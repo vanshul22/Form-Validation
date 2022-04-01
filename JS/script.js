387e93c0933909a35a3fce3ada1d4c120e321c98
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-01 15:50:57 +0530

    Version 3.2.1 : Added submit event listener.

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,15 @@
 Website Name - Form Validation
 Name : Vanshul Kesharwani
 Date : 01/04/2022
-Version : 3.1.1
+Version : 3.2.1
 Email : vkvanshulkesharwani54@gmail.com
 Description : This is a Web app for validating a form with Regular Expression of JavaScript.
 */
+
+// taking default values for form correction.
+let userNameValid = false;
+let emailValid = false;
+let phoneValid = false;
 
 // Taken all required variables from their ID.
 let userName = document.getElementById("userName");
@@ -24,9 +29,11 @@ function blurUserName() {
     if (re.test(str)) {
         console.log("your name is valid");
         userName.classList.remove('is-invalid');
+        userNameValid = true;
     } else {
         console.log("Your name is invalid");
         userName.classList.add('is-invalid');
+        userNameValid = false;
     };
 };
 
@@ -38,9 +45,11 @@ function blurPhone() {
     if (re.test(str)) {
         console.log("your phone is valid");
         phone.classList.remove('is-invalid');
+        phoneValid = true;
     } else {
         console.log("Your phone is invalid");
         phone.classList.add('is-invalid');
+        phoneValid = false;
     };
 };
 
@@ -52,8 +61,26 @@ function blurEmail() {
     if (re.test(str)) {
         console.log("your email is valid");
         email.classList.remove('is-invalid');
+        emailValid = true;
     } else {
         console.log("Your email is invalid");
         email.classList.add('is-invalid');
+        emailValid = false;
+    };
+};
+
+
+// validating all form data from submit ID.
+let submit = document.getElementById("submit");
+submit.addEventListener("click", submitData);
+
+// Adding Submit function
+function submitData(e) {
+    e.preventDefault();
+    console.log(userNameValid, emailValid, phoneValid);
+    if (userNameValid, emailValid, phoneValid) {
+        console.log("values are correct");
+    } else {
+        console.log("values are not correct");
     };
 };
